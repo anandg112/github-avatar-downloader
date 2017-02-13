b105330e0e729aa71a8191fb0b0e678f83f2a312
Anand <anandg112@gmail.com>
2017-02-13 21:55:08 +0000

    added function call

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,15 @@
 var request = require('request');
 
+const GITHUB_USER = "anandg112";
+const GITHUB_TOKEN = "5b9a10c8f21730e6c7b61d335023341ae27ce327";
+
 console.log("Welcome to the GitHub Avatar Downloader!");
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  // ...
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
