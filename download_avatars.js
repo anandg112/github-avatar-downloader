0dda5709ffa49ff3e2c9bc227e0c425d0182caea
Anand <anandg112@gmail.com>
2017-02-13 22:01:48 +0000

    Added request URL

--- download_avatars.js
+++ download_avatars.js
@@ -6,7 +6,9 @@ const GITHUB_TOKEN = "5b9a10c8f21730e6c7b61d335023341ae27ce327";
 console.log("Welcome to the GitHub Avatar Downloader!");
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+//GET /repos/:owner/:repo/contributors
+var requestURL = "https://" + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+console.log(requestURL);
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
